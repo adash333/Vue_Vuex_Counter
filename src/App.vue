20223d33ea90710fb52c90424dca0d6ffa035693
adash333 <adash333@hotmail.com>
2019-01-30 05:14:28 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-title>Vue Vuex Counter</v-toolbar-title>
+      </v-toolbar>
+
+      <v-content>
+        <v-container fluid fill-height> <counter></counter> </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Counter
+  }
+};
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card>
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+        aspect-ratio="2.75"
+      ></v-img>
+
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">{{ count }}</h3>
+        </div>
+      </v-card-title>
+
+      <v-card-actions>
+        <v-btn v-on:click="decrement" color="error">-1</v-btn>
+        <v-btn v-on:click="increment" color="primary">+1</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Counter",
+  computed: {
+    ...mapGetters({
+      count: "getCount"
+    })
+  },
+  methods: {
+    ...mapActions(["increment", "decrement"])
+  }
+};
+</script>
